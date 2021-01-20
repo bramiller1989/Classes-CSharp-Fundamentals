e68f7076c7bb8aab9fb4c5698cfbb8d3d09c1fdb
bramiller1989 <bramiller1989@gmail.com>
2021-01-19 19:30:32 -0600

    create person

--- Program.cs
+++ Program.cs
@@ -11,14 +11,16 @@ namespace Classes
         {
             Console.WriteLine("My name is " + FirstName + " " + LastName);
         }
-
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
         }
     }
 }
